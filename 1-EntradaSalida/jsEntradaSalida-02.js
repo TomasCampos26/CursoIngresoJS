561a4c0759ba3e48efc77612605123880e3bb027
TomasCampos26 <tomascampos@gmail.com>
2023-01-31 14:48:58 -0300

    mi primer commit

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,12 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'
+Campos Tomás - Div X*/
 function mostrar()
 {
-	alert("ok");
-}
+	let nombre;
 
+	nombre=prompt("Ingrese su nombre");
+	
+	alert(nombre);
+}
